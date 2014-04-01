58db8b542a47256d20afbbeedfb68159e9b0292e
Nathalia Franca <nath.campreguer@gmail.com>
2014-04-01 17:28:39 -0300

    Initial configuration.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GeonetworkApp::Application.config.secret_key_base = '18175bb2b983f4af79a20da0c29bddee6f47be7debc10aef78980879bbef04d0b8f2ad49ca5ef66e1ee85be4382abb4fc2f7c6a3bfa8844b7971b32acb008645'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GeonetworkApp::Application.config.session_store :cookie_store, key: '_geonetwork-app_session'
