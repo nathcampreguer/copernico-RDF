9cf5565064b549c5db57f79623887e47efefc4b7
Thiago Alves <thiagofalves2@gmail.com>
2014-04-07 17:48:50 -0300

    Basic search done but needs fixing on tests

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,45 +3,50 @@ require 'nokogiri'
 
 class SearchController < ApplicationController
   def index
+    url = 'http://boldo.caiena.net:8080/geonetwork/srv/eng/'
+
+    # request & response for metadata summaries
     builder_for_summary = Nokogiri::XML::Builder.new do |xml|
       xml.request {
         xml.any params[:search_field]
       }
     end
 
-    url = "http://boldo.caiena.net:8080/geonetwork/srv/eng/xml.search"
-
     response = HTTP.with_headers(content_type: "application/xml")
-                   .post(url, body: builder_for_summary.to_xml).response
+                   .post("#{url}xml.search",
+                   body: builder_for_summary.to_xml).response
 
+    # needed variables for retrieving the full metadata
     @ids = get_metadata_id(response.body)
     @schemas = get_schema(response.body)
     @count = get_metadata_count(response.body)
 
+    # array of resulting metada
+    @metadata_info = []
+    index = 0
 
-    @title = []
-    i = 0
-
-    while i < @count.to_i do
+    # request & response for full metadata
+    while index < @count.to_i do
         builder_for_metadata = Nokogiri::XML::Builder.new do |xml|
           xml.request {
-            xml.id @ids[i]
+            xml.id @ids[index]
           }
         end
 
-        url2 = "http://boldo.caiena.net:8080/geonetwork/srv/eng/xml.metadata.get"
-
         response2 = HTTP.with_headers(content_type: "application/xml")
-                       .post(url2, body: builder_for_metadata.to_xml).response
+                       .post("#{url}xml.metadata.get",
+                       body: builder_for_metadata.to_xml).response
 
-        if @schemas[i] == "iso19139"
-          @title << get_metadata_title_19139(response2.body)
-        elsif @schemas[i] == "iso19115"
-          @title << get_metadata_title_19115(response2.body)
+        # gets
+        if @schemas[index] == "iso19139"
+          @metadata_info << get_metadata_19139(response2.body)
+        elsif @schemas[index] == "iso19115"
+          @metadata_info << get_metadata_19115(response2.body)
         elsif
-          @title << get_metadata_title_fgdcstd_dublincore(response2.body)
+          @metadata_info << get_metadata_fgdcstd_dublincore(response2.body)
         end
-      i += 1
+
+      index += 1
     end
   end
 
@@ -66,27 +71,34 @@ class SearchController < ApplicationController
       xml.xpath("//response/@to").text
     end
 
-    def get_metadata_title_19139(data)
+    def get_metadata_19139(data)
       xml = Nokogiri::XML(data)
-      xml.xpath("//gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString").text
+      keywords = []
+      title = xml.xpath("//gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString").text
+      author = xml.xpath("//gmd:contact/gmd:CI_ResponsibleParty/gmd:individualName/gco:CharacterString").text
+      date = xml.xpath("//gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:date/gmd:CI_Date/gmd:date/gco:Date").text
+      xml.xpath("//gmd:descriptiveKeywords/gmd:MD_Keywords/gmd:keyword/gco:CharacterString").each{|elem| keywords << elem.text}
+      metadata = [title, author, date, keywords]
     end
 
-    def get_metadata_title_19115(data)
+    def get_metadata_19115(data)
       xml = Nokogiri::XML(data)
-      xml.xpath("//resTitle").text
+      keywords = []
+      title = xml.xpath("//resTitle").text
+      author = xml.xpath("//mdContact/rpIndName").text
+      date = xml.xpath("//dataIdInfo/idCitation/resRefDate/refDate").text
+      xml.xpath("//descKeys/keyword").each{|elem| keywords << elem.text}
+      metadata = [title, author, date, keywords]
+
     end
 
-    def get_metadata_title_fgdcstd_dublincore(data)
+    def get_metadata_fgdcstd_dublincore(data)
       xml = Nokogiri::XML(data)
-      xml.xpath("//title").text
+      keywords = []
+      title = xml.xpath("//title").text
+      author = xml.xpath("//origin").text
+      date = xml.xpath("//pubdate").text
+      xml.xpath("//keywords/theme/themekey").each{|elem| keywords << elem.text}
+      metadata = [title, author, date, keywords]
     end
-
-    #iso19139
-    #gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString
-
-    #fgdc-std / dublin-core
-    #title
-
-    #iso19115
-    #resTitle
 end
